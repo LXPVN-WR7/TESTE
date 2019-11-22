94a4fb023890bfc06a410b47a5adf07031555439
FIC <FIC@info.local>
2019-11-22 15:56:06 -0300

    Update no Projeto GUFUS

--- REACT/gufos/src/index.js
+++ REACT/gufos/src/index.js
@@ -1,10 +1,13 @@
 // Arquivo de configuração de rotas 
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// Importando as dependências da Biblioteca MDB
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'; // Importando dependências
 
